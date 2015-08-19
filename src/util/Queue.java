bcc690ea177bcdedbb5bf89fe1608c40c0eeee36
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-19 01:08:28 -0400

    Add stack to util

---
+++ src/util/Queue.java
@@ -0,0 +1,7 @@
+package util;
+
+/**
+ * Created by guangshuozang on 8/19/15.
+ */
+public class Queue {
+}
---
+++ src/util/Stack.java
@@ -0,0 +1,27 @@
+package util;
+
+/**
+ * Created by guangshuozang on 8/19/15.
+ */
+public class Stack {
+    LinkedListNode top;
+    Object pop(){
+        if(this.top != null){
+            Object item = top.getKey();
+            top = top.getNext();
+            return item;
+        }
+        return null;
+    }
+    void push(Object item){
+        LinkedListNode newTop = new LinkedListNode(item);
+        newTop.setNext(top);
+        this.top = newTop;
+    }
+    Object peek(){
+        return top.getKey();
+    }
+    @Override public String toString(){
+        return this.top.getKey().toString();
+    }
+}
