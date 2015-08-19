2c17b90cd50d3e2fe700f564c47079b268d9f101
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-19 01:21:28 -0400

    add queue to util

--- src/util/Queue.java
+++ src/util/Queue.java
@@ -4,4 +4,25 @@ package util;
  * Created by guangshuozang on 8/19/15.
  */
 public class Queue {
+    LinkedListNode first, last;
+    void enqueue(Object item){
+        if(first != null){
+            LinkedListNode temp;
+            temp = first;
+            first = new LinkedListNode(item);
+            first.setNext(temp);
+        }else{
+            first = new LinkedListNode(item);
+            last = first;
+        }
+    }
+    Object dequeue(){
+        if(last != null){
+            Object item = last.getKey();
+            last = null;
+            return item;
+        }else{
+            return null;
+        }
+    }
 }
