90bb165fbda06564c9a6c3864b9233a92e02e013
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-20 18:57:01 -0400

    Ex1 S2

--- src/ChapterThree/Exc1S2.java
+++ src/ChapterThree/Exc1S2.java
@@ -5,5 +5,19 @@ package ChapterThree;
  * This is too large implement for a single interview question. I will finish it later, say, this weekend
  */
 public class Exc1S2 {
+    int[] buffer;
+    class stackInfo{
+        int capacity;
+        int stackNum;
+        int pointer;
+    }
+    public void push(){
 
+    }
+    public int pop(){
+        return -1;
+    }
+    public void shift(int position){
+
+    }
 }
