a69a7d57443b30b71e5c770acda1e81d05d4ac79
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-19 21:05:11 -0400

    update

--- src/ChapterThree/Exc1S1.java
+++ src/ChapterThree/Exc1S1.java
@@ -1,7 +1,20 @@
 package ChapterThree;
 
+
 /**
  * Created by guangshuozang on 8/17/15.
  */
 public class Exc1S1 {
+    class stackByArray{
+        int stackSize = 100;
+        int[] buffer = new int[stackSize * 3];
+        int[] stackPointer
+
+        void push(int stackNum, int value) throws Exception {
+            if(stackPointer)
+        }
+    }
+
+
+
 }
