77ca3caf73f4a711406e9d563a11d9ca2594da6a
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-21 21:56:23 -0400

    pseudocode

---
+++ src/ChapterThree/Exc3S1.java
@@ -0,0 +1,30 @@
+package ChapterThree;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Created by guangshuozang on 8/20/15.
+ */
+public class Exc3S1 {
+    ArrayList listOfStack;
+    int threshold;
+    int stackCount;
+    int stackPointer;
+    public void push(int item){
+        //if no stack or stack is full then create a new, attach it at he end of the list
+         //and set current stack
+        //else regular push to current stack
+
+    }
+    public int pop(){
+        //if current stack is empty look at last stack
+          //if still empty throw exception
+        //regular pop
+    }
+    public int popAt(int stackPointer){
+        //if stack is not empty
+        //pop
+        //else exception
+    }
+}
