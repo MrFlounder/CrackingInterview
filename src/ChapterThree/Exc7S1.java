c419e21a26132e5fc07a34e4582575f9c3442b35
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-24 00:18:18 -0400

    update

--- src/ChapterThree/Exc7S1.java
+++ src/ChapterThree/Exc7S1.java
@@ -1,6 +1,7 @@
 package ChapterThree;
 
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
@@ -8,12 +9,13 @@ import java.util.Queue;
  * I use tree queues, one for dog, one for cat, one for animal
  */
 public class Exc7S1 {
-    Queue cat;
-    Queue dog;
-    Queue animal;
+    LinkedList<Animal> cat;
+    LinkedList<Animal> dog;
+    LinkedList<Animal> animal;
     Exc7S1(){
-        this.cat = new Queue();
-
+        this.cat = new LinkedList<Animal>();
+        this.dog = new LinkedList<Animal>();
+        this.animal = new LinkedList<Animal>();
     }
     abstract class Animal{
         String name;
@@ -33,8 +35,35 @@ public class Exc7S1 {
             super(n);
         }
     }
-    public void enqueue(Animal a){
+    public void enqueue(Animal a) throws Exception{
         if(a instanceof cat )
-
+            cat.addLast(a);
+        else if(a instanceof dog)
+            dog.addLast(a);
+        else throw new Exception("Error animal not recognized");
+        animal.addLast(a);
+    }
+    public Animal dequeueCat(){
+            if (animal.poll() instanceof cat)
+            return cat.poll();
+            else{
+                animal.poll();
+                animal.addFirst(dog.peekFirst());
+                return cat.poll();
+            }
+    }
+    public Animal dequeueDog(){
+            if(animal.poll() instanceof dog)
+            return dog.poll();
+        else{
+                animal.poll();
+                animal.addFirst(cat.peekFirst());
+                return dog.poll();
+            }
+    }
+    public Animal dequeue(){
+        if(animal.poll() instanceof cat)
+            return cat.poll();
+        else return dog.poll();
     }
 }
