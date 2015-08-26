f14d753c65e4395b3f84f41f01a90e2b2d8070ee
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-26 16:58:18 -0400

    DFS

---
+++ src/ChapterFour/AdditionalQuestion.java
@@ -0,0 +1,8 @@
+package ChapterFour;
+
+/**
+ * Created by guangshuozang on 8/26/15.
+ * To Implement a binary tree having add, remove, find, insert, 3 kinds of traversal
+ */
+public class AdditionalQuestion {
+}
---
+++ src/util/DFS.java
@@ -0,0 +1,32 @@
+package util;
+
+/**
+ * Created by guangshuozang on 8/26/15.
+ * This is pseudocode, not used anywhere
+ */
+public class DFS {
+    /*
+    void search(Node root){
+        if(root == null) return;
+        visit(root);
+        root.visited = true;
+        foreach(Node n in root.adjacent){
+            if(n.visited == false){
+                search(n);
+            }
+        }
+    }
+
+    void searchWithStack(Node root){
+        if(root == null) return;
+        visit(root);
+        root.visited = true;
+        stack.push(root);
+        searchWithStack(root.smallestUnvisitedAdjacent);
+        stack.pop;
+    }
+
+    */
+
+}
+
