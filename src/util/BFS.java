cb52da82110ad62c45f304dca13e6ecb93ab47d8
Guangshuo Zang <k.zanggs@gmail.com>
2015-08-26 22:20:24 -0400

    BFS

---
+++ src/util/BFS.java
@@ -0,0 +1,25 @@
+package util;
+
+/**
+ * Created by guangshuozang on 8/26/15.
+ */
+public class BFS {
+    void search(Node root){
+        /*
+        Queue queue = new Queue();
+        root.visited = true;
+        visit(root);
+        queue.enqueue(root);
+        while(!queue.isEmpty()){
+            Node r = queue.dequeue();
+            foreach(Node n in r.adjacent){
+                if(n.visited == false){
+                    visit(n);
+                    n.visited = true;
+                    queue.enqueue(n);
+                }
+            }
+        }*/
+    }
+
+}
