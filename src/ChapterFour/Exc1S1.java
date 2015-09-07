c7e599da7780bcc2fac46bbc98211cd333e25915
Guangshuo Zang <k.zanggs@gmail.com>
2015-09-07 18:17:00 -0400

    update

--- src/ChapterFour/Exc1S1.java
+++ src/ChapterFour/Exc1S1.java
@@ -4,7 +4,7 @@ import tree.util.*;
 /**
  * Created by guangshuozang on 8/30/15.
  * This is recursive solution
- * Time O(N*logN)????????????????
+ * Time O(N*logN)
  *
  */
 public class Exc1S1 {
@@ -20,6 +20,26 @@ public class Exc1S1 {
             return false;
         else
             return isBalanced(entry.left)&&isBalanced(entry.right);
+
+    }
+    public int getHeight2(TreeNode entry){
+        if(entry == null)
+            return 0;
+        int leftHeight = 0;
+        int rightHeigth = 0;
+        leftHeight = getHeight(entry.left);
+        rightHeigth = getHeight(entry.right);
+        if(leftHeight==-1&&rightHeigth==-1)
+            return -1;
+        if(Math.abs(leftHeight-rightHeigth)>1)
+            return -1;
+        return Math.max(leftHeight,rightHeigth)+1;
+    }
+    public boolean isBalanced2(TreeNode entry){
+        if(getHeight(entry)==-1)
+            return false;
+        else
+            return true;
 
     }
 }
