0af65951e009e5a189aec541316076c59dc90624
Guangshuo Zang <k.zanggs@gmail.com>
2015-09-07 19:21:08 -0400

    DFS solution

--- src/ChapterFour/Exc2S1.java
+++ src/ChapterFour/Exc2S1.java
@@ -9,14 +9,17 @@ import java.util.Stack;
 /**
  * Created by guangshuozang on 9/7/15.
  */
+//DFS
 public class Exc2S1 {
     public boolean DFSearch(Graph g, GraphNode start, GraphNode end){
         Stack<GraphNode> stack = new Stack<GraphNode>();
-        start.state = State.Visiting;
-        stack.push(start);
-        for(GraphNode n : start.getAdjacent()){
-
-
+        if(start==end)
+            return true;
+        start.state = State.Visited;
+        for (GraphNode n : start.getAdjacent()) {
+            if (n.state == State.Unvisited)
+                DFSearch(g, n, end);
         }
+        return false;
     }
 }
