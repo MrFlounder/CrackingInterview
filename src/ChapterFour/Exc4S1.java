506693b591d374bc39c3c91b0d180f1a3065f42d
Guangshuo Zang <k.zanggs@gmail.com>
2015-09-08 18:48:06 -0400

    Exc4 S1

--- src/ChapterFour/Exc4S1.java
+++ src/ChapterFour/Exc4S1.java
@@ -1,7 +1,34 @@
 package ChapterFour;
 
+import tree.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * Created by guangshuozang on 9/7/15.
+ * 1. BFS
  */
 public class Exc4S1 {
+    public ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root){
+        ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>();
+        LinkedList<TreeNode> current = new LinkedList<TreeNode>();
+        if(root!=null){
+            current.add(root);
+        }
+        while(current.size()>0){
+            result.add(current);
+            LinkedList<TreeNode> parents = current;
+            current = new LinkedList<TreeNode>();
+            for(TreeNode parent: parents){
+                if(parent.left!=null){
+                    current.add(parent.left);
+                }
+                if(parent.right!=null){
+                    current.add(parent.right);
+                }
+            }
+        }
+        return result;
+    }
 }
