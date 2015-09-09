d6fcce72777bd504dfe877e542db3836ef60de38
Guangshuo Zang <k.zanggs@gmail.com>
2015-09-08 20:58:49 -0400

    solution 2

---
+++ src/ChapterFour/Exc5S2.java
@@ -0,0 +1,23 @@
+package ChapterFour;
+
+import tree.util.TreeNode;
+
+/**
+ * Created by guangshuozang on 9/8/15.
+ */
+public class Exc5S2 {
+    boolean checkBST(TreeNode n){
+        return checkBST(n,null,null);
+    }
+    boolean checkBST(TreeNode n, Integer min, Integer max){
+        if(n==null)
+            return true;
+        if(min !=null && n.val <= min)||(max!=null&&n.val>max){
+            return false;
+        }
+        if(!checkBST(n.left,min,n.val)||!checkBST(n.right,n.val,max)){
+            return false;
+        }
+        return true;
+    }
+}
