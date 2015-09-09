7cadc983a08c72290bcc33c5d13f6aec9451cc1c
Guangshuo Zang <k.zanggs@gmail.com>
2015-09-08 21:07:54 -0400

    update

--- src/ChapterFour/Exc5S2.java
+++ src/ChapterFour/Exc5S2.java
@@ -12,7 +12,7 @@ public class Exc5S2 {
     boolean checkBST(TreeNode n, Integer min, Integer max){
         if(n==null)
             return true;
-        if(min !=null && n.val <= min)||(max!=null&&n.val>max){
+        if((min !=null && n.val <= min)||(max!=null&&n.val>max)){
             return false;
         }
         if(!checkBST(n.left,min,n.val)||!checkBST(n.right,n.val,max)){
